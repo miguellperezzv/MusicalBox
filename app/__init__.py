3ac0a352b6113ecbba5867598d627b56994f0c09
miguellperezzv <42900928+miguellperezzv@users.noreply.github.com>
2021-07-09 16:56:02 -0500

    sqlalchemy + marshmallow

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from db import db, ma 
 from config import  DevelopmentConfig 
 from flask import Flask
 from store.views import home
@@ -15,6 +16,13 @@ if __name__ == "__main__":
     app = Flask(__name__)
     app.jinja_env.trim_blocks = True
     app.config.from_object(DevelopmentConfig)
+
+    db.init_app(app)
+    ma.init_app(app)
+
+    with app.app_context():   #el contexto es la DB, el serializable 
+        db.create_all()
+
     for url,  blueprint in ACTIVE_ENDPOINTS:
         app.register_blueprint(blueprint, url_prefix=url)
     db = SQLAlchemy(app)
